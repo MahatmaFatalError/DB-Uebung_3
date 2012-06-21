565a5aedb6a5d9c10b6f4e5f1520cb4435660ee8
Mahatma_Fatal_Error <mahatmafatal@gmail.com>
2012-06-21 23:28:42 +0200

    firs try join

--- src/de/dhbw/db2/youdb/sql/operator/EquiJoin.java
+++ src/de/dhbw/db2/youdb/sql/operator/EquiJoin.java
@@ -1,8 +1,11 @@
 package de.dhbw.db2.youdb.sql.operator;
 
-import de.dhbw.db2.youdb.storage.AbstractRecord;
+import java.util.Arrays;
 
-@SuppressWarnings("unused")
+import de.dhbw.db2.youdb.storage.AbstractRecord;
+import de.dhbw.db2.youdb.storage.Record;
+import de.dhbw.db2.youdb.storage.types.AbstractSQLValue;
+
 public class EquiJoin extends BinaryOperator {
 	
 	private int leftAtt=0;
@@ -23,8 +26,37 @@ public class EquiJoin extends BinaryOperator {
 	@Override
 	public AbstractRecord next() {
 		//TODO: implement this method
+		AbstractRecord joinedRec = null;
+		while(true){
+			AbstractRecord leftRec = this.leftChild.next();
+			AbstractSQLValue leftValue = leftRec.getValue(leftAtt);
+			
+			while(true){
+				AbstractRecord rightRec = this.rightChild.next();
+				if(rightRec == null){
+					break;
+				}
+				
+				AbstractSQLValue rightValue = rightRec.getValue(rightAtt);
+				
+				if(leftValue.compareTo(rightValue) == 1){
+					
+					AbstractSQLValue[] leftValues = leftRec.getValues();
+					AbstractSQLValue[] rightValues = rightRec.getValues();
+					
+					AbstractSQLValue[] newValues = concat(leftValues, rightValues);
+					
+					joinedRec = new Record(newValues.length);
+					joinedRec.append(leftRec).append(rightRec);
+					
+					return joinedRec;					
+				}
+			}
+			
+			break;
+		}		
 		
-		return null;
+		return joinedRec;
 	}
 
 	@Override
@@ -32,5 +64,11 @@ public class EquiJoin extends BinaryOperator {
 		this.leftChild.close();
 		this.rightChild.close();
 	}
+	
+	private static <T> T[] concat(T[] first, T[] second) {
+		  T[] result = Arrays.copyOf(first, first.length + second.length);
+		  System.arraycopy(second, 0, result, first.length, second.length);
+		  return result;
+		}
 
 }
